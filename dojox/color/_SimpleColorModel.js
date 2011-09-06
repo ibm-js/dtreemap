016c502a101225c63f9b0f9db7ed857a7ca59a0f
cjolif <cjolif@gmail.com>
2011-09-06 18:08:02 +0200

    fix capitalization for non class module

--- dojox/color/_SimpleColorModel.js
+++ dojox/color/_SimpleColorModel.js
@@ -1,5 +1,5 @@
 define(["dojo/_base/array", "dojo/_base/declare", "dojox/color"], 
-	function(arr, declare, Color){
+	function(arr, declare, color){
 	
 	return declare("dojox.color._ColorModelBase", null, {
 		// summary:
@@ -29,10 +29,10 @@ define(["dojo/_base/array", "dojo/_base/declare", "dojox/color"],
 				var hsl = startColor.toHsl();
 				hsl.s = 100; 
 				hsl.l = 85; 
-				this._startColor = Color.fromHsl(hsl.h, hsl.s, hsl.l);
+				this._startColor = color.fromHsl(hsl.h, hsl.s, hsl.l);
 				this._startColor.a = startColor.a;
 				hsl.l = 15;
-				this._endColor = Color.fromHsl(hsl.h, hsl.s, hsl.l);
+				this._endColor = color.fromHsl(hsl.h, hsl.s, hsl.l);
 				this._endColor.a = startColor.a;
 			}
 		},
@@ -61,7 +61,7 @@ define(["dojo/_base/array", "dojo/_base/declare", "dojox/color"],
 			var s = this._getInterpoledValue(hslFrom.s, hslTo.s, completion);
 			var l = this._getInterpoledValue(hslFrom.l, hslTo.l, completion);
 			var a = this._getInterpoledValue(this._startColor.a, this._endColor.a, completion);
-			var c = Color.fromHsl(h, s, l);
+			var c = color.fromHsl(h, s, l);
 			c.a = a;
 			return c;
 		}
