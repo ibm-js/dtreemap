c88e4f4caa9d7eb0820e7b357b96b5e6d521a357
U-IBM-KQ5OP6BM73M\cjolif <cjolif@IBM-KQ5OP6BM73M.(none)>
2011-09-28 18:01:51 +0200

    simplify imports

--- dojox/treemap/TreeMap.js
+++ dojox/treemap/TreeMap.js
@@ -1,7 +1,7 @@
 define(["dojo/_base/array", "dojo/_base/lang", "dojo/_base/declare", "dojo/_base/event", "dojo/_base/Color",
 		"dojo/_base/Deferred", "dojo/on", "dojo/query", "dojo/dom-construct", "dojo/dom-geometry", "dojo/dom-class", "dojo/dom-style",
 		"./_utils", "dijit/_WidgetBase", "dojox/widget/_Invalidating", "dojox/widget/Selection",
-		"dojo/has", "dojo/_base/sniff"],
+		"dojo/_base/sniff"],
 	function(arr, lang, declare, event, Color, Deferred, on, query, domConstruct, domGeom, domClass, domStyle,
 		utils, _WidgetBase, _Invalidating, Selection, has){
 
